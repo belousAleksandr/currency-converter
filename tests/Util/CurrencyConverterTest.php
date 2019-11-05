fe8e5eea52294b5375f444dba1d02d16d2417b5d
belousaleksandr <belous803@gmail.com>
2019-11-05 23:15:59 +0200

    Fix tests related CurrencyConverter

--- tests/Util/CurrencyConverterTest.php
+++ tests/Util/CurrencyConverterTest.php
@@ -4,39 +4,106 @@ declare(strict_types=1);
 
 namespace App\Tests\Util;
 
+use App\Entity\ExchangeRate;
 use App\Exception\InvalidCurrencySource;
+use App\Repository\ExchangeRateRepository;
 use App\Util\CurrencyConverter;
 use App\Util\CurrencyExchangeRequest;
 use App\Util\CurrencySources\CurrencySourceInterface;
+use Doctrine\ORM\EntityManager;
 use PHPUnit\Framework\TestCase;
 
 class CurrencyConverterTest extends TestCase
 {
+    public function testExchangeValidRequestFromDatabase()
+    {
+        $testSourceName = 'testSource';
+        $testAmount = 20;
+        $course = 10;
+
+        $requestMock = $this->createMock(CurrencyExchangeRequest::class);
+        $requestMock->method('getAmount')->willReturn($testAmount);
+        $requestMock->method('getSource')->willReturn($testSourceName);
+        $requestMock->method('getCurrencyTo')->willReturn('USD');
+        $requestMock->method('getCurrencyFrom')->willReturn('UAH');
+
+        $exchangeRateMock = $this->createMock(ExchangeRate::class);
+        $exchangeRateMock->method('getCourse')->willReturn($course);
+
+        $exchangeRateRepositoryMock = $this->createMock(ExchangeRateRepository::class);
+        $exchangeRateRepositoryMock
+            ->method('findSourceCourse')
+            ->with($requestMock->getSource(), $requestMock->getCurrencyFrom(), $requestMock->getCurrencyTo())
+            ->willReturn($exchangeRateMock);
+
+        $entityManagerMock = $this->createMock(EntityManager::class);
+        $entityManagerMock->method('getRepository')
+            ->with(ExchangeRate::class)
+            ->willReturn($exchangeRateRepositoryMock);
+
+        $currencyConverter = new CurrencyConverter([], $entityManagerMock);
+        $result = $currencyConverter->exchange($requestMock);
+
+        $this->assertEquals(200, $result);
+    }
+
     public function testExchangeValidRequest()
     {
         $testSourceName = 'testSource';
-        $testValue = 20;
-        $sourceMock = $this->createMock(CurrencySourceInterface::class);
-        $sourceMock->method('exchange')->willReturn($testValue);
-        $sourceMock->method('getName')->willReturn($testSourceName);
+        $testAmount = 20;
+        $course = 10;
+
         $requestMock = $this->createMock(CurrencyExchangeRequest::class);
+        $requestMock->method('getAmount')->willReturn($testAmount);
         $requestMock->method('getSource')->willReturn($testSourceName);
-        $currencyConverter = new CurrencyConverter([$sourceMock]);
+        $requestMock->method('getCurrencyTo')->willReturn('USD');
+        $requestMock->method('getCurrencyFrom')->willReturn('UAH');
+
+        $entityManagerMock = $this->createMock(EntityManager::class);
+
+        $exchangeRateRepositoryMock = $this->createMock(ExchangeRateRepository::class);
+        $exchangeRateRepositoryMock
+            ->method('findSourceCourse')
+            ->with($requestMock->getSource(), $requestMock->getCurrencyFrom(), $requestMock->getCurrencyTo())
+            ->willReturn(null);
+
+        $entityManagerMock->method('getRepository')
+            ->with(ExchangeRate::class)
+            ->willReturn($exchangeRateRepositoryMock);
+
+        $sourceMock = $this->createMock(CurrencySourceInterface::class);
+        $sourceMock->method('getCourse')->willReturn($course);
+        $sourceMock->method('getName')->willReturn($testSourceName);
+
+        $currencyConverter = new CurrencyConverter([$sourceMock], $entityManagerMock);
         $result = $currencyConverter->exchange($requestMock);
 
-        $this->assertEquals($testValue, $result);
+        $this->assertEquals(200, $result);
     }
 
     public function testInvalidSource()
     {
+        $testAmount = 20;
+        $course = 10;
+
+        $entityManagerMock = $this->createMock(EntityManager::class);
+        $exchangeRateRepository = $this->createMock(ExchangeRateRepository::class);
+        $entityManagerMock->method('getRepository')
+            ->with(ExchangeRate::class)
+            ->willReturn($exchangeRateRepository);
         $this->expectException(InvalidCurrencySource::class);
-        $testValue = 20;
+
         $sourceMock = $this->createMock(CurrencySourceInterface::class);
-        $sourceMock->method('exchange')->willReturn($testValue);
+        $sourceMock->method('getCourse')->willReturn($course);
         $sourceMock->method('getName')->willReturn('testSource');
+
         $requestMock = $this->createMock(CurrencyExchangeRequest::class);
+        $requestMock->method('getAmount')->willReturn($testAmount);
         $requestMock->method('getSource')->willReturn('testSource2');
-        $currencyConverter = new CurrencyConverter([$sourceMock]);
+        $requestMock->method('getCurrencyTo')->willReturn('USD');
+        $requestMock->method('getCurrencyFrom')->willReturn('UAH');
+
+        $currencyConverter = new CurrencyConverter([$sourceMock], $entityManagerMock);
         $currencyConverter->exchange($requestMock);
     }
 }
